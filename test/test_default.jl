e5fb573350206ae0ba770896ede772eb5da8d5b3
Olivier Cots <66357348+ocots@users.noreply.github.com>
2023-04-04 20:59:19 +0200

    foo

--- test/test_default.jl
+++ test/test_default.jl
@@ -54,34 +54,6 @@ function test_default()
         @test CTBase.__init_interpolation() isa Function
     end
 
-    @testset "Default value of the grid size for the direct shooting method" begin
-        @test CTBase.__grid_size_direct_shooting() isa Integer
-    end
-
-    @testset "Default value of the penalty term for the direct shooting method" begin
-        @test CTBase.__penalty_term_direct_shooting() isa Real
-    end
-
-    @testset "Default value of the maximum number of iterations for the direct shooting method" begin
-        @test CTBase.__max_iter_direct_shooting() isa Integer
-    end
-
-    @testset "Default value of the absolute tolerance for the direct shooting method" begin
-        @test CTBase.__abs_tol_direct_shooting() isa Real
-    end
-
-    @testset "Default value of the optimality tolerance for the direct shooting method" begin
-        @test CTBase.__opt_tol_direct_shooting() isa Real
-    end
-
-    @testset "Default value of the stagnation tolerance for the direct shooting method" begin
-        @test CTBase.__stagnation_tol_direct_shooting() isa Real
-    end
-
-    @testset "Default value of the grid size for the direct method" begin
-        @test CTBase.__grid_size_direct() isa Integer
-    end
-
     @testset "Default value of the print level of ipopt for the direct method" begin
         @test CTBase.__print_level_ipopt() isa Integer
         @test CTBase.__print_level_ipopt() ≤ 12
