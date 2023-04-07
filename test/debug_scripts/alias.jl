e4e52e4c9ccdaca927a64accc99bbf9a8cf72412
Jean Luc Szpyrka <jean-luc@szpyrka.fr>
2023-04-07 12:58:02 +0200

    added new Rⁿ syntax for control_vector and state_vector

--- test/debug_scripts/alias.jl
+++ test/debug_scripts/alias.jl
@@ -15,11 +15,27 @@ tf = 1.0
 @def verbose_threshold=80 debug=true begin
     t0, variable
     t ∈ [ t0, tf ], time
-    x ∈ R^3, state
+    x ∈ R⁹, state
 
-    v = x₂
+    a = x₁
+    b = x₂
+    c = x₃
+    d = x₄
+    e = x₅
+    f = x₆
+    g = x₇
+    h = x₈
+    i = x₉
 
-    0  ≤ v(t) ≤ 1
+    0  ≤ a(t) ≤ 1
+    0  ≤ b(t) ≤ 1
+    0  ≤ c(t) ≤ 1
+    0  ≤ d(t) ≤ 1
+    0  ≤ e(t) ≤ 1
+    0  ≤ f(t) ≤ 1
+    0  ≤ g(t) ≤ 1
+    0  ≤ h(t) ≤ 1
+    0  ≤ i(t) ≤ 1
 
     v(tf) -> min
 
