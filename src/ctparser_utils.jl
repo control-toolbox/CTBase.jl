ed2c5bb8921f34ba185910acdb841c77ac612ed4
jcaillau <jean-baptiste.caillau@inria.fr>
2023-04-08 12:41:09 +0200

    added x(t) and u(t) in constrainnt-type

--- src/ctparser_utils.jl
+++ src/ctparser_utils.jl
@@ -249,6 +249,9 @@ julia> constraint_type(:( u[1:2](t) ), t, t0, tf, x, u)
 julia> constraint_type(:( u[1](t) ), t, t0, tf, x, u)
 (:control_range, Index(1))
 
+julia> constraint_type(:( u(t) ), t, t0, tf, x, u)
+(:control_range, nothing)
+
 julia> constraint_type(:( 2u[1](t)^2 ), t, t0, tf, x, u)
 (:control_fun, :(2 * u[1] ^ 2))
 
@@ -257,6 +260,9 @@ julia> constraint_type(:( x[1:2](t) ), t, t0, tf, x, u)
 
 julia> constraint_type(:( x[1](t) ), t, t0, tf, x, u)
 (:state_range, Index(1))
+
+julia> constraint_type(:( x(t) ), t, t0, tf, x, u)
+(:state_range, nothing)
 
 julia> constraint_type(:( 2x[1](t)^2 ), t, t0, tf, x, u)
 (:state_fun, :(2 * x[1] ^ 2))
@@ -289,10 +295,12 @@ constraint_type(e, t, t0, tf, x, u) =
         [ false, false, true , false, false, false ] => @match e begin
             :( $v[$i:$j]($s) ) => (v == u && s == t ) ? (:control_range, i:j     ) : (:other, nothing)
             :( $v[$i   ]($s) ) => (v == u && s == t ) ? (:control_range, Index(i)) : (:other, nothing)
+            :( $v($s)        ) => (v == u && s == t ) ? (:control_range, nothing ) : (:other, nothing)
             _                  =>                                                    (:control_fun, replace_call(e, u, t, u)) end
         [ false, false, false, true , false, false ] => @match e begin
             :( $y[$i:$j]($s) ) => (y == x && s == t ) ? (:state_range  , i:j     ) : (:other, nothing)
             :( $y[$i   ]($s) ) => (y == x && s == t ) ? (:state_range  , Index(i)) : (:other, nothing)
+            :( $y($s)        ) => (y == x && s == t ) ? (:state_range  , nothing ) : (:other, nothing)
             _                  =>                                                    (:state_fun  , replace_call(e, x, t, x)) end
         [ false, false, true , true , false, false ] => begin
             ee = replace_call(e , u, t, u)
@@ -343,9 +351,9 @@ input_line_type(e) =
         :($t ∈ R, variable )     => (e_variable, [t])
         :($t, variable )         => (e_variable, [t])
         :($a = $b)               => (e_alias, [a, b])
-        :($a -> begin min end)   => (e_objective_min, [a])
+        :($a -> begin min end)   => (e_objective_min, [a]) # debug: not allowed, remove
         :($a → min)              => (e_objective_min, [a])
-        :($a -> begin max end)   => (e_objective_max, [a])
+        :($a -> begin max end)   => (e_objective_max, [a]) # debug: not allowed, remove
         :($a → max)              => (e_objective_max, [a])
         :($e => ($n))            => (e_named_constraint, [e, n])
         :($e , ($n))             => (e_named_constraint, [e, n])
