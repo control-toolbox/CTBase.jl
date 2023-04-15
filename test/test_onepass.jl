2aa19bd4093c9be23530f0122f4b6a4d46b1ab41
jcaillau <jean-baptiste.caillau@inria.fr>
2023-04-15 16:55:13 +0200

    def1

--- test/test_onepass.jl
+++ test/test_onepass.jl
@@ -1,16 +1,16 @@
 # test onepass
+t0 = 0
+o = @def1 t ∈ [ t0, t0 + 4 ], time
+
 function test_onepass()
 
-t0 = 0
-
-o = @def1 t ∈ [ t0, t0 + 4 ], time
 @test o.initial_time == t0
 @test o.final_time == t0 + 4 
 @test o.parsed.t == :t
 
 o = @def1 begin
-    λ ∈ R^2,  variable
-    tf ∈ R,  variable
+    λ ∈ R^2, variable
+    tf ∈ R, variable
 end
 @def1 o t ∈ [ 0, tf ], time
 @test :λ ∈ keys(o.parsed.vars)
@@ -19,8 +19,8 @@ end
 @test o.final_time == nothing
 
 o = @def1 begin
-    t0 ∈ R,  variable
-end
+    t0 ∈ R, variable
+    end
 @def1 o t ∈ [ t0, 1 ], time
 @test :t0 ∈ keys(o.parsed.vars)
 @test o.initial_time == nothing
@@ -29,7 +29,7 @@ end
 @def1 o begin
     x ∈ R, state
     u ∈ R, control
-end
+    end
 @test o.state_dimension == 1
 @test o.control_dimension == 1
 @test o.parsed.x == :x
@@ -38,7 +38,7 @@ end
 @def1 o begin
     x ∈ R^3, state
     u ∈ R^2, control
-end
+    end
 @test o.state_dimension == 3
 @test o.control_dimension == 2
 
