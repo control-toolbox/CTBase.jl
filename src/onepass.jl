0f9d0f9c9ed8818933ec3917d3311bec69fa80ba
jcaillau <jean-baptiste.caillau@inria.fr>
2023-04-15 18:28:53 +0200

    def1, no eval

--- src/onepass.jl
+++ src/onepass.jl
@@ -1,6 +1,6 @@
 # onepass
 # todo: add aliases doing, if they exist, an expansion / replace pass on the e
-# at the beginning of parse!
+# at the beginning of parse
 
 """
 $(TYPEDSIGNATURES)
@@ -32,35 +32,39 @@ end
 
 p_variable(ocp, v, q=1; log=false) = begin
     log && println("variable: $v, dim: $q")
-    __ocp.parsed.vars[v] = q
-    LineNumberNode(0, "variable: $v, dim: $q")
+    code = :( $ocp.parsed.vars[v] = $q )
+    code
 end
 
 p_time(ocp, t, t0, tf; log=false) = begin
     log && println("time: $t, initial time: $t0, final time: $tf")
-    __ocp.parsed.t = t
-    __ocp.parsed.t0 = t0
-    __ocp.parsed.tf = tf
     tt = QuoteNode(t)
-    cond = (t0 ∈ keys(__ocp.parsed.vars), tf ∈ keys(__ocp.parsed.vars))
-    @match cond begin
-        (false, false) => :( time!($ocp, [ $t0, $tf ] , String($tt)) )
-        (false, true ) => :( time!($ocp, :initial, $t0, String($tt)) )
-        (true , false) => :( time!($ocp, :final  , $tf, String($tt)) )
-        _              => throw("both initial and final time cannot be variable")
-    end
-end
+    code = :( $ocp.parsed.t = $t )
+    code = Expr(:block, code, :( $ocp.parsed.t0 = $t0 ))
+    code = Expr(:block, code, :( $ocp.parsed.tf = $tf ))
+    code = Expr(:block, code, quote
+        cond = ($t0 ∈ keys($ocp.parsed.vars), $tf ∈ keys($ocp.parsed.vars))
+        @match cond begin
+            (false, false) => time!($ocp, [ $t0, $tf ] , String($tt))
+            (false, true ) => time!($ocp, :initial, $t0, String($tt))
+            (true , false) => time!($ocp, :final  , $tf, String($tt))
+            _              => throw("both initial and final time cannot be variable")
+        end
+    end)
+    code
 
 p_state(ocp, x, n=1; log=false) = begin
     log && println("state: $x, dim: $n")
-    __ocp.parsed.x = x
-    :( state!($ocp, $n) ) # debug: add state name
+    code = :( $ocp.parsed.x = $x )
+    code = Expr(:block, state!($ocp, $n)) # debug: add state name
+    code
 end
 
 p_control(ocp, u, m=1; log=false) = begin
     log && println("control: $u, dim: $m")
-    __ocp.parsed.u = u
-    :( control!($ocp, $m) ) # debug: add control name
+    code = :( $ocp.parsed.u = $u )
+    code = Expr(:block, control!($ocp, $m)) # debug: add control name
+    code
 end
 
 """
