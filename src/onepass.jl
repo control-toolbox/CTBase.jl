4c8d3d782e4d74792008d7ee0b05d998e1ba0ad5
jcaillau <jean-baptiste.caillau@inria.fr>
2023-04-19 13:42:26 +0200

    onepass: non inc

--- src/onepass.jl
+++ src/onepass.jl
@@ -9,13 +9,13 @@ $(TYPEDEF)
 
 """
 @with_kw mutable struct ParsingInfo
-    aliases::Dict{Symbol, Any}=Dict{Symbol, Any}()
-    vars::Dict{Symbol, Integer}=Dict{Symbol, Integer}()
     t::Union{Symbol, Nothing}=nothing
     t0::Union{Real, Symbol, Expr, Nothing}=nothing
     tf::Union{Real, Symbol, Expr, Nothing}=nothing
     x::Union{Symbol, Nothing}=nothing
     u::Union{Symbol, Nothing}=nothing
+    aliases::Dict{Symbol, Any}=Dict{Symbol, Any}() # this Any could refined
+    vars::Dict{Symbol, Any}=Dict{Symbol, Any}() # idem
 end
 
 """
@@ -28,121 +28,105 @@ Foo
 Foo
 ```
 """
-parse(ocp, e; log=true) = @match e begin
-    :( $v ∈ R^$q, variable ) => p_variable(ocp, v, q; log)
-    :( $v ∈ R   , variable ) => p_variable(ocp, v   ; log)
-    :( $t ∈ [ $t0, $tf ], time ) => p_time(ocp, t, t0, tf; log)
-    :( $x ∈ R^$n, state ) => p_state(ocp, x, n; log)
-    :( $x ∈ R   , state ) => p_state(ocp, x   ; log)
-    :( $u ∈ R^$m, control ) => p_control(ocp, u, m; log)
-    :( $u ∈ R   , control ) => p_control(ocp, u   ; log)
-    :( $a = $e1 ) => p_alias(ocp, a, e1; log)
-    :( $x'($t) == $e1 ) => p_dynamics(ocp, x, t, e1; log)
-    :( $e1 == $e2 ) => p_constraint_eq(ocp, e1, e2; log)
-    :( ∫($e1) → min ) => p_objective(ocp, e1, :min; log)
-    :( ∫($e1) → max ) => p_objective(ocp, e1, :max; log)
+parse!(p, ocp, e; log=false) = @match e begin
+    :( $v ∈ R^$q, variable ) => p_variable!(p, ocp, v, q; log)
+    :( $v ∈ R   , variable ) => p_variable!(p, ocp, v   ; log)
+    :( $t ∈ [ $t0, $tf ], time ) => p_time!(p, ocp, t, t0, tf; log)
+    :( $x ∈ R^$n, state ) => p_state!(p, ocp, x, n; log)
+    :( $x ∈ R   , state ) => p_state!(p, ocp, x   ; log)
+    :( $u ∈ R^$m, control ) => p_control!(p, ocp, u, m; log)
+    :( $u ∈ R   , control ) => p_control!(p, ocp, u   ; log)
+    :( $a = $e1 ) => p_alias!(p, ocp, a, e1; log)
+    :( $x'($t) == $e1 ) => p_dynamics!(p, ocp, x, t, e1; log)
+    :( $e1 == $e2 ) => p_constraint_eq!(p, ocp, e1, e2; log)
+    :( ∫($e1) → min ) => p_objective!(p, ocp, e1, :min; log)
+    :( ∫($e1) → max ) => p_objective!(p, ocp, e1, :max; log)
     _ =>
 
     if e isa LineNumberNode
         e
     elseif (e isa Expr) && (e.head == :block)
-        Expr(:block, map(e -> parse(ocp, e), e.args)...)
+	code = LineNumberNode(0, "start")
+	for ee ∈ e.args
+	    code = Expr(:block, code, parse!(p, ocp, ee; log))
+	end
+        code
     else
         throw("syntax error")
     end
 end
 
-p_variable(ocp, v, q=1; log=false) = begin
+p_variable!(p, ocp, v, q=1; log=false) = begin
     log && println("variable: $v, dim: $q")
-    vv = QuoteNode(v)
-    :( $ocp.parsed.vars[$vv] = $(esc(q)) )
+    p.vars[v] = q
+    :( LineNumberNode(1, "variable: $v, dim: $(esc(q))") )
 end
 
-p_alias(ocp, a, e; log=false) = begin
+p_alias!(p, ocp, a, e; log=false) = begin
     log && println("alias: $a = $e")
-    aa = QuoteNode(a)
-    ee = QuoteNode(e)
-    :( $ocp.parsed.aliases[$aa] = $ee )
+    p.aliases[a] = e
+    :( LineNumberNode(2, "alias: $a = $e") )
 end
 
-p_time(ocp, t, t0, tf; log=false) = begin
+p_time!(p, ocp, t, t0, tf; log=false) = begin
     log && println("time: $t, initial time: $t0, final time: $tf")
+    p.t = t 
+    p.t0 = t0
+    p.tf = tf
     tt = QuoteNode(t)
-    tt0 = QuoteNode(t0)
-    ttf = QuoteNode(tf)
-    quote
-        $ocp.parsed.t = $tt
-        $ocp.parsed.t0 = $tt0
-        $ocp.parsed.tf = $ttf
-        @match ($tt0 ∈ keys($ocp.parsed.vars), $ttf ∈ keys($ocp.parsed.vars)) begin
-            (false, false) => time!($ocp, [ $(esc(t0)), $(esc(tf)) ], String($tt))
-            (false, true ) => time!($ocp, :initial, $(esc(t0)), String($tt))
-            (true , false) => time!($ocp, :final  , $(esc(tf)), String($tt))
-            _              => throw("parsing error: both initial and final time " *
-	                            "cannot be variable")
-        end
+    @match (t0 ∈ keys(p.vars), tf ∈ keys(p.vars)) begin
+        (false, false) => :( time!($ocp, [ $(esc(t0)), $(esc(tf)) ], String($tt)) )
+        (false, true ) => :( time!($ocp, :initial, $(esc(t0)), String($tt)) :)
+        (true , false) => :( time!($ocp, :final  , $(esc(tf)), String($tt)) :)
+        _              => throw("parsing error: both initial and final time " *
+	                        "cannot be variable")
     end
 end
 
-p_state(ocp, x, n=1; log=false) = begin
+p_state!(p, ocp, x, n=1; log=false) = begin
     log && println("state: $x, dim: $n")
-    xx = QuoteNode(x)
-    quote
-        $ocp.parsed.x = $xx
-        state!($ocp, $(esc(n))) # todo: add state name
-    end
+    p.x = x
+    :( state!($ocp, $(esc(n))) ) # todo: add state name
 end
 
-p_control(ocp, u, m=1; log=false) = begin
+p_control!(p, ocp, u, m=1; log=false) = begin
     log && println("control: $u, dim: $m")
-    uu = QuoteNode(u)
-    quote
-        $ocp.parsed.u = $uu
-        control!($ocp, $(esc(m))) # todo: add control name
-    end
+    p.u = u
+    :( control!($ocp, $(esc(m))) ) # todo: add control name
 end
 
-p_constraint_eq(ocp, e1, e2; log) = begin
+p_constraint_eq!(p, ocp, e1, e2; log) = begin
     log && println("constraint: $e1 == $e2")
-    ee1 = QuoteNode(e1)
-    quote
-        @match constraint_type($ee1,
-	    $ocp.parsed.t,
-	    $ocp.parsed.t0,
-	    $ocp.parsed.tf,
-	    $ocp.parsed.x,
-	    $ocp.parsed.u) begin
-	    (:initial, nothing) => constraint!($ocp, :initial,      $(esc(e2)))
-	    (:initial, val    ) => constraint!($ocp, :initial, val, $(esc(e2)))
-	    (:final  , nothing) => constraint!($ocp, :final  ,      $(esc(e2)))
-	    (:final  , val    ) => constraint!($ocp, :final  , val, $(esc(e2)))
-	    _ => throw("syntax error")
-	end
+    @match constraint_type($e1, p.t, p.t0, p.tf, p.x, p.u) begin
+        (:initial, nothing) => :( constraint!($ocp, :initial,      $(esc(e2))) )
+	(:initial, val    ) => :( constraint!($ocp, :initial, val, $(esc(e2))) )
+	(:final  , nothing) => :( constraint!($ocp, :final  ,      $(esc(e2))) )
+	(:final  , val    ) => :( constraint!($ocp, :final  , val, $(esc(e2))) )
+	_ => throw("syntax error")
     end
 end
 
-p_dynamics(ocp, x, t, e; log) = begin
+p_dynamics!(p, ocp, x, t, e; log) = begin
     log && println("dynamics: $x'($t) == $e")
-    xx = QuoteNode(x)
-    tt = QuoteNode(t)
+    ( x ≠ p.x ) && throw("dynamics: wrong state")
+    ( t ≠ p.t ) && throw("dynamics: wrong time")
+    e = replace_call(e, p.t)
     gs = gensym()
-    quote # debug: let seems to be escaped; but vars local to @match seem not...
-        ( $xx ≠ $ocp.parsed.x ) && throw("dynamics: wrong state")
-        ( $tt ≠ $ocp.parsed.t ) && throw("dynamics: wrong time")
-        function $gs($ocp.parsed.x, $ocp.parsed.u)
-	    $( replace_call(esc(e), ocp.parsed.t) )
+    quote
+        function $gs($(p.x), $(p.u))
+	    $(esc(e))
 	end
 	constraint!($ocp, :dynamics, $gs)
     end
 end
 
-p_objective(ocp, e, type; log) = begin
+p_objective!(p, ocp, e, type; log) = begin
     log && println("objective: ∫($e) → $type")
-    e = replace_call(e, :t) # debug
+    e = replace_call(e, p.t)
     ttype = QuoteNode(type)
     gs = gensym()
     quote
-        function $gs($ocp.parsed.x, $ocp.parsed.u)
+        function $gs($(p.x), $(p.u))
 	    $(esc(e))
 	end
 	objective!($ocp, :lagrange, $gs, $ttype)
@@ -161,7 +145,8 @@ Foo
 """
 macro def1(ocp, e)
     #esc( parse(ocp, e; log=true) )
-    parse(esc(ocp), e; log=true)
+    p = ParsingInfo()
+    parse!(p, esc(ocp), e; log=true)
 end
 
 """
