9c18dad09c3946b9d91defaecb9caf1613e145e9
Olivier Cots <66357348+ocots@users.noreply.github.com>
2024-09-10 21:04:54 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,10 +2,13 @@ using Documenter
 using CTBase
 using DocumenterMermaid
 
+repo_url = "github.com/control-toolbox/CTBase.jl"
+
 makedocs(;
     warnonly = [:cross_references, :autodocs_block],
     sitename = "CTBase.jl",
     format = Documenter.HTML(
+        repolink = "https://"*repo_url,
         prettyurls = false,
         size_threshold_ignore = ["api.md", "dev.md"],
         assets = [
@@ -17,4 +20,6 @@ makedocs(;
     checkdocs = :none,
 )
 
-deploydocs(repo = "github.com/control-toolbox/CTBase.jl.git", devbranch = "main")
+deploydocs(;
+    repo=repo_url*".git", devbranch="main"
+)
