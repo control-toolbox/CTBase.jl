f4d5b39017e444550b272dd820c99c0d2f278bb7
Olivier Cots <olivier.cots@irit.fr>
2025-05-30 14:11:42 +0200

    formatting

---
+++ test/test_code_quality.jl
@@ -0,0 +1,29 @@
+function test_code_quality()
+    @testset verbose = true "Code quality" begin
+        @testset "Aqua" begin
+            Aqua.test_all(
+                CTBase;
+                ambiguities=false,
+                #stale_deps=(ignore=[:SomePackage],),
+                deps_compat=(ignore=[:LinearAlgebra, :Unicode],),
+                piracies=true,
+            )
+            # do not warn about ambiguities in dependencies
+            Aqua.test_ambiguities(CTBase)
+        end
+
+        # @testset "JET" begin
+        #     JET.test_package(CTBase; target_defined_modules=true)
+        # end
+
+        @testset "JuliaFormatter" begin
+            @test JuliaFormatter.format(CTBase; verbose=true, overwrite=false)
+        end
+
+        @testset "Doctests" begin
+            Documenter.doctest(CTBase)
+        end
+    end
+
+    return nothing
+end
